f8f3eb8dfcf91373265a668b28a154098ec34f12
Legion <minhnhph22720@fpt.edu.vn>
2024-03-06 13:29:11 +0700

    loc

--- src/main/resources/static/admin/AngularJS/banHangTaiQuay.js
+++ src/main/resources/static/admin/AngularJS/banHangTaiQuay.js
@@ -84,7 +84,7 @@ app.controller("banhang-ctrl", function ($scope, $http) {
                     donGiaSauGiam: c.donGiaSauGiam
                 })
             })
-            $scope.donHangAdd.email = "baphuoc03@gmail.com"
+            $scope.donHangAdd.email = "minhisme0510@gmail.com"
             $scope.donHangAdd.thanhPhoName = "a"
             $scope.donHangAdd.thanhPhoCode = 1
             $scope.donHangAdd.quanHuyenName = "a"
@@ -659,5 +659,93 @@ app.controller("banhang-ctrl", function ($scope, $http) {
             $scope.chuaThanhToan.checkButton();
         }
     }
+    $scope.getPropertiesInFilter = function (){
+        $http.get("/admin/mau-sac/find-all").then(r =>{
+            $scope.mauSac = r.data;
+        }).catch( e => console.log(e))
 
+        $http.get("/admin/chat-lieu/find-all").then(r =>{
+            $scope.chatLieu = r.data;
+        }).catch( e => console.log(e))
+
+        $http.get("/admin/thuong-hieu/find-all").then(r =>{
+            $scope.thuongHieu = r.data;
+        }).catch( e => console.log(e))
+
+        $http.get("/admin/xuat-xu/find-all").then(r =>{
+            $scope.xuatXu = r.data;
+        }).catch( e => console.log(e))
+
+        $http.get("/admin/kieu-dang/find-all").then(r =>{
+            $scope.kieuDang = r.data;
+        }).catch( e => console.log(e))
+    }
+    $scope.getPropertiesInFilter();
+
+    $scope.filter = function (filterData){
+        for (const [key, value] of Object.entries(filterData)) {
+            if(value.length==0) delete filterData[key]
+        }
+        console.log(filterData)
+        console.log(isNaN(filterData.giaBan))
+        if(filterData.giaBan != undefined){
+            if(isNaN(filterData.giaBan)){
+                alertify.error("Giá min phải là số!!")
+                return
+            }else{
+                if(filterData.giaBan < 10000){
+                    alertify.error("Giá min phải > 10.000đ!!")
+                    return
+                }
+            }
+        }
+        if(filterData.giaMax != undefined){
+            if(isNaN(filterData.giaMax)){
+                alertify.error("Giá max phải là số!!")
+                return
+            }else{
+                if(filterData.giaMax > 100000000){
+                    alertify.error("Giá max phải < 100.000.000đ !!")
+                    return
+                }else{
+                    if(filterData.giaBan != undefined){
+                        if(parseFloat(filterData.giaBan) > parseFloat(filterData.giaMax)){
+                            console.log("max",parseFloat(filterData.giaMax))
+                            console.log("min",parseFloat(filterData.giaBan))
+                            alertify.error("Giá max phải > giá min!!")
+                            return
+                        }
+                    }
+                }
+            }
+        }
+
+        $scope.pageNumber = 0
+        $scope.filterDto = filterData
+        $scope.pageNumbers = []
+        $http.post("/admin/san-pham/filter",$scope.filterDto).then(r => {
+            if(Object.keys( $scope.filterDto ).length>0){
+                document.getElementById('lengthFilter').innerText = Object.keys( $scope.filterDto ).length
+            }else{
+                document.getElementById('lengthFilter').innerText = ""
+            }
+            $scope.items = r.data.content;
+            $scope.totalPage = r.data.totalPages;
+            $scope.getPageNumbers(r.data.totalPages)
+            isfilter = true;
+        }).catch(e => console.log(e))
+    }
+    $scope.clearFilter = function (){
+
+        $scope.pageNumber = 0
+        $scope.pageNumbers = []
+        $http.get("/admin/san-pham/get-all").then(r => {
+            document.getElementById('lengthFilter').innerText = ""
+            $scope.items = r.data.content;
+            $scope.getPageNumbers(r.data.totalPages)
+            $scope.filterData = {}
+            $scope.filterDto = {}
+            isfilter = false;
+        }).catch(e => console.log(e))
+    }
 })
